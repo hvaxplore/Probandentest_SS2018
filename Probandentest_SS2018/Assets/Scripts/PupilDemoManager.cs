17c88a0179e1d85e4c1b7d4b7416823bf28eab5d
Exaii2 <adrian.voegtle@hs-furtwangen.de>
2018-05-30 16:00:45 +0200

    Added Demo Manager
    Allows calibration and start of scene

---
+++ Probandentest_SS2018/Assets/Scripts/PupilDemoManager.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Pupil;
+
+public class PupilDemoManager : MonoBehaviour 
+{
+	public Calibration.Mode calibrationMode = Calibration.Mode._2D;
+	public List<GameObject> gameObjectsToEnable;
+
+	GameObject cameraObject;
+	Text calibrationText;
+
+	void Start()
+	{	
+		PupilTools.OnConnected += OnConnected;
+		PupilTools.OnDisconnecting += OnDisconnected;
+		PupilTools.OnCalibrationStarted += OnCalibtaionStarted;
+		PupilTools.OnCalibrationEnded += OnCalibrationEnded;
+		PupilTools.OnCalibrationFailed += OnCalibrationFailed;
+	
+		PupilSettings.Instance.currentCamera = GetComponentInChildren<Camera> ();
+		cameraObject = PupilSettings.Instance.currentCamera.gameObject;
+
+		ResetCalibrationText ();
+	}
+
+	void ResetCalibrationText()
+	{
+		if (calibrationText == null)
+			calibrationText = cameraObject.GetComponentInChildren<Text> ();
+
+		if (PupilTools.Connection.isAutorun)
+			calibrationText.text = "Connecting to pupil.";
+		else
+			calibrationText.text = "Select PupilGazeTracker and\npress 'Start' in the Inspector GUI\nto connect to Pupil.";
+	}
+
+	void OnDisconnected()
+	{
+		ResetCalibrationText ();
+	}
+
+	void OnConnected()
+	{
+		calibrationText.text = "Success";
+
+		PupilTools.CalibrationMode = calibrationMode;
+
+		Invoke ("ShowCalibrate", 1f);
+	}
+
+	void ShowCalibrate()
+	{
+		calibrationText.text = "Press 'c' to start calibration.";
+	}
+
+	void OnCalibtaionStarted()
+	{
+		cameraObject.SetActive (true);
+		PupilSettings.Instance.currentCamera = cameraObject.GetComponent<Camera> ();
+		calibrationText.text = "";
+			
+		foreach (GameObject go in gameObjectsToEnable) 
+		{
+			go.SetActive (false);
+		}
+	}
+		
+	void OnCalibrationEnded()
+	{
+		calibrationText.text = "Calibration ended.";
+
+		Invoke ("StartDemo", 1f);
+	}
+
+	void OnCalibrationFailed()
+	{
+		calibrationText.text = "Calibration failed\nPress 'c' to start it again.";
+	}
+
+	void StartDemo()
+	{
+		foreach (GameObject go in gameObjectsToEnable) 
+		{
+			go.SetActive (true);
+		}
+		cameraObject.SetActive (false);
+	}
+
+	void Update()
+	{
+        /*
+		if (Input.GetKeyUp (KeyCode.S)) 
+			StartDemo ();
+        */
+	}
+
+    public void StartCalibrationProcess()
+    {
+        PupilTools.StartCalibration();
+    }
+
+	void OnApplicationQuit()
+	{
+		PupilTools.OnConnected -= OnConnected;
+		PupilTools.OnDisconnecting -= OnDisconnected;
+		PupilTools.OnCalibrationStarted -= OnCalibtaionStarted;
+		PupilTools.OnCalibrationEnded -= OnCalibrationEnded;
+		PupilTools.OnCalibrationFailed -= OnCalibrationFailed;
+	}
+}
